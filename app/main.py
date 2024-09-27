8bcc8e2aff90779175e0d92496c20386c83f6399
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2024-09-27 17:03:34 +0300

    implemented class Car and class CarWashStation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,55 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(
+        self, comfort_class: int,
+        clean_mark: int,
+        brand: str
+    ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+        self, distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
+    ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(
+        self: "CarWashStation",
+        car: "Car"
+    ) -> float:
+        washing_price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
+        )
+        return round(washing_price, 1)
+
+    def serve_cars(self: "CarWashStation", cars: list[Car]) -> float:
+        income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return income
+
+    def wash_single_car(self: "CarWashStation", car: "Car") -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self: "CarWashStation", rate: int) -> None:
+        updated_count_of_ratings = self.count_of_ratings + 1
+        updated_average_rating = (
+            (self.average_rating * self.count_of_ratings + rate)
+            / updated_count_of_ratings
+        )
+        self.count_of_ratings = updated_count_of_ratings
+        self.average_rating = round(updated_average_rating, 1)
